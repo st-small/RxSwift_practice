872a94edec70243dc45dbf515a95971deae19d6a
Stanly Shiyanovskiy <st-small@rambler.ru>
2018-06-05 20:16:43 +0300

    added project files

--- FirstPlayground.playground/Contents.swift
+++ FirstPlayground.playground/Contents.swift
@@ -1,5 +1,6 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import PlaygroundSupport
 
 var str = "Hello, playground"
+
---
+++ RxSwift/RxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwift
+//
+//  Created by Stanly Shiyanovskiy on 05.06.18.
+//  Copyright © 2018 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
