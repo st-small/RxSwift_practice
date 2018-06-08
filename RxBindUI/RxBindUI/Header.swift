3111552360632991b232a9ec40161febc5252c2d
Stanly Shiyanovskiy <st-small@rambler.ru>
2018-06-08 12:54:22 +0300

    finished BindUI

---
+++ RxBindUI/RxBindUI/Header.swift
@@ -0,0 +1,14 @@
+//
+//  Header.swift
+//  RxBindUI
+//
+//  Created by Stanly Shiyanovskiy on 07.06.18.
+//  Copyright © 2018 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class Header: UICollectionReusableView {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+}
