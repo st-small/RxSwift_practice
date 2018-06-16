76c84dac3c4a868124d7270a86951c82b4b2098b
Stanly Shiyanovskiy <st-small@rambler.ru>
2018-06-16 21:10:10 +0300

    added new project RxMVVMSample

---
+++ RxMVVMSample/RxMVVMSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxMVVMSample
+//
+//  Created by Stanly Shiyanovskiy on 16.06.18.
+//  Copyright © 2018 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
